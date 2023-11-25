cc0ad83b8ee1faa4ff97af2fb4259351e0d3db6f
Frank Cooke <frankcooke79@gmail.com>
2023-11-24 19:02:21 -0500

    refactor: title styling on login page again...

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,9 +4,13 @@ import { Spinner } from '~/components/Spinner';
 
 export default function Login() {
   return (
-    <main className="pt-20 flex h-screen w-full flex-col items-center bg-gray-100 px-2 md:px-0">
-      <h1 className="leading-[4rem] animate-bg-move mb-4 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-200 via-neutral-700 to-gray-200
-       bg-clip-text text-center text-6xl font-black text-transparent"
+    <main
+      className="flex h-screen w-full flex-col items-center bg-gray-100
+        px-2 pt-32 md:px-0 md:pt-44"
+    >
+      <h1
+        className="animate-bg-move mb-4 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-200 via-neutral-700 to-gray-200 bg-clip-text
+       text-center text-6xl font-black leading-[4rem] text-transparent"
       >
         Next.js 14 TodoMVC
       </h1>
